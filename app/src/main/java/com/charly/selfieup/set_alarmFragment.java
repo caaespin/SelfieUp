b830b5ed9017a5f27acd6e74f3615ed4049d4b14
Charly <caaespin@ucsc.edu>
2016-07-01 15:43:33 -0700

    Fixed some problems with the expandable button

--- app/src/main/java/com/charly/selfieup/set_alarmFragment.java
+++ app/src/main/java/com/charly/selfieup/set_alarmFragment.java
@@ -33,20 +33,52 @@ public class set_alarmFragment extends Fragment {
         //Images to be used for the days
         final ImageView monday = (ImageView) rootView.findViewById(R.id.monday);
         final ImageView tuesday = (ImageView) rootView.findViewById(R.id.tuesday);
+        final ImageView wednesday = (ImageView) rootView.findViewById(R.id.wednesday);
+        final ImageView thursday = (ImageView) rootView.findViewById(R.id.thursday);
+        final ImageView friday = (ImageView) rootView.findViewById(R.id.friday);
+        final ImageView saturday = (ImageView) rootView.findViewById(R.id.saturday);
+        final ImageView sunday = (ImageView) rootView.findViewById(R.id.sunday);
+        final ImageView close = (ImageView) rootView.findViewById(R.id.close);
         final FloatingActionButton fabA = (FloatingActionButton) rootView.findViewById(R.id.fabtoolbar_fab);
         final FABToolbarLayout layout = (FABToolbarLayout) rootView.findViewById(R.id.fabtoolbar);
         fabA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Make the days of the week and the close tiles
                 TextDrawable monday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
-                        .buildRoundRect("M", Color.parseColor("#6633ff"), 10); // radius in px
-
+                        .buildRoundRect("M", Color.parseColor("#3333ff"), 10);
                 TextDrawable tuesday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
                         .buildRoundRect("T", Color.parseColor("#3333ff"), 10);
-                //ImageView image = (ImageView) rootView.findViewById(R.id.monday);
+                TextDrawable wednesday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
+                        .buildRoundRect("W", Color.parseColor("#3333ff"), 10);
+                TextDrawable thursday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
+                        .buildRoundRect("Th", Color.parseColor("#3333ff"), 10);
+                TextDrawable friday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
+                        .buildRoundRect("F", Color.parseColor("#3333ff"), 10);
+                TextDrawable saturday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
+                        .buildRoundRect("S", Color.parseColor("#3333ff"), 10);
+                TextDrawable sunday_drawable = TextDrawable.builder().beginConfig().bold().endConfig()
+                        .buildRoundRect("Su", Color.parseColor("#3333ff"), 10);
+                TextDrawable close_drawable = TextDrawable.builder().beginConfig().bold().withBorder(7).endConfig()
+                        .buildRound("X", Color.parseColor("#ffb433"));
+                //Set the days and close tiles as the drawables.
                 monday.setImageDrawable(monday_drawable);
                 tuesday.setImageDrawable(tuesday_drawable);
+                wednesday.setImageDrawable(wednesday_drawable);
+                thursday.setImageDrawable(thursday_drawable);
+                friday.setImageDrawable(friday_drawable);
+                saturday.setImageDrawable(saturday_drawable);
+                sunday.setImageDrawable(sunday_drawable);
+                close.setImageDrawable(close_drawable);
+                //Show the final layout
                 layout.show();
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+               layout.hide();
             }
         });
         return rootView;

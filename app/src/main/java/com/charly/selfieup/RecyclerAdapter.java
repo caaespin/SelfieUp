2ef4dd6933b762cea0424dbe534930e98424d18f
Charly <caaespin@ucsc.edu>
2016-07-05 21:06:49 -0700

    more fun UI stuff

--- app/src/main/java/com/charly/selfieup/RecyclerAdapter.java
+++ app/src/main/java/com/charly/selfieup/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.charly.selfieup;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -38,14 +39,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         Alarm mAlarm = alarmDataSource.get(position);
         String[] time = mAlarm.getTime().split(":");
         TextDrawable tdHour = TextDrawable.builder().beginConfig().bold().endConfig()
-                .buildRoundRect(time[0], Color.parseColor("#3333ff"), 10);;
-        TextDrawable tdMinute = TextDrawable.builder().beginConfig().bold().endConfig()
-                .buildRoundRect(time[1] + "pm", Color.parseColor("#3333ff"), 10);;
-        TextDrawable tdDays = TextDrawable.builder().beginConfig().endConfig()
+                .buildRoundRect(time[0]+":"+time[1]+"pm", Color.parseColor("#3333ff"), 10);;
+        //TextDrawable tdMinute = TextDrawable.builder().beginConfig().bold().endConfig()
+        //        .buildRoundRect(time[1], Color.parseColor("#3333ff"), 10);;
+        TextDrawable tdDays = TextDrawable.builder().beginConfig().bold().endConfig()
                 .buildRoundRect("M T W Th F S Su", Color.parseColor("#3333ff"), 10);;
         holder.vHour.setImageDrawable(tdHour);
-        holder.vMinute.setImageDrawable(tdMinute);
-        holder.vDays.setImageDrawable(tdDays);
+        //holder.vMinute.setImageDrawable(tdMinute);
+        //holder.vDays.setImageDrawable(tdDays);
 
     }
 
@@ -63,10 +64,13 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         public ViewHolder(View cardView) {
             super(cardView);
             vHour = (ImageView) cardView.findViewById(R.id.hourIs);
-            vMinute = (ImageView) cardView.findViewById(R.id.minuteIs);
+            //vMinute = (ImageView) cardView.findViewById(R.id.minuteIs);
             vDays = (ImageView) cardView.findViewById(R.id.daysAre);
 
         }
+    }
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 
 }

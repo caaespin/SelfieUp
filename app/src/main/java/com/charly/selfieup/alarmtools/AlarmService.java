bab36080a2b7e4b724b8c2ea4aee7ff6c4d01c6c
Charly <caaespin@ucsc.edu>
2016-07-12 18:39:25 -0700

    Added AlarmService

---
+++ app/src/main/java/com/charly/selfieup/alarmtools/AlarmService.java
@@ -0,0 +1,48 @@
+package com.charly.selfieup.alarmtools;
+
+/**
+ * Created by Charly on 7/12/16.
+ */
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.charly.selfieup.R;
+import com.charly.selfieup.set_alarm;
+
+public class AlarmService extends IntentService {
+    private NotificationManager alarmNotificationManager;
+
+    public AlarmService() {
+        super("AlarmService");
+    }
+
+    @Override
+    public void onHandleIntent(Intent intent) {
+        sendNotification("Wake Up! Wake Up!");
+    }
+
+    private void sendNotification(String msg) {
+        Log.d("AlarmService", "Preparing to send notification...: " + msg);
+        alarmNotificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, set_alarm.class), 0);
+
+        NotificationCompat.Builder alamNotificationBuilder = new NotificationCompat.Builder(
+                this).setContentTitle("Alarm").setSmallIcon(R.drawable.ic_event_black_24dp)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
+                .setContentText(msg);
+
+
+        alamNotificationBuilder.setContentIntent(contentIntent);
+        alarmNotificationManager.notify(1, alamNotificationBuilder.build());
+        Log.d("AlarmService", "Notification sent.");
+    }
+}
+
